51a15c77e20c36c3c90c40d67348c85bf5fc7e7d
kundan-whitehat-kk <Kundan_kk52@outlook.com>
2017-11-19 09:43:35 +0530

    Initial commit

---
+++ app/src/main/java/com/kk/solution/dev/chatfirebase/Friends.java
@@ -0,0 +1,26 @@
+package com.kk.solution.dev.chatfirebase;
+
+/**
+ * Created by Kundan on 30/10/2017
+ */
+
+public class Friends {
+
+    public String date;
+
+    public Friends(){
+
+    }
+
+    public Friends(String date) {
+        this.date = date;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
